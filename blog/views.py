9435bd20c2c3e480743a902a25ccd1d8c124cef7
Oswaldo <adico.nike99@gmail.com>
2019-07-04 10:24:48 -0500

    Templates modificados para mostrar post desde la base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    try:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    except Exception as ex:
+        print("\nExcepcion en views Post: {0}".format(type(ex).__name__))
+    return render(request, 'blog/post_list.html',{'posts': posts})
     
